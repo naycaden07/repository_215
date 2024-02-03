b81f127e92c0f065f40159cc3cdd7bb67a743f29
Nay Fernandes <cadennay17@gmail.com>
2024-02-03 10:40:54 +0530

    new

---
+++ nay_215_new/src/pack_1/catepillar.java
@@ -0,0 +1,13 @@
+package pack_1;
+
+public class catepillar {
+	int moves,cabagge;
+	
+	
+	catepillar(int moves,int cabbages, int cabbage)
+	{
+		this.moves=moves;
+		this.cabagge=cabbages;
+	}
+
+}
